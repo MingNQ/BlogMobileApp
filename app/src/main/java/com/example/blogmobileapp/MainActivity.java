6c42ee09b9e3fe9ef9d2a16747678c8f1998be18
Ming <minhlaminh01@gmail.com>
2025-02-11 22:38:08 +0700

    Test connect database

--- app/src/main/java/com/example/blogmobileapp/MainActivity.java
+++ app/src/main/java/com/example/blogmobileapp/MainActivity.java
@@ -1,32 +1,103 @@
 package com.example.blogmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
-    private FirebaseAnalytics mFirebaseAnalytics;
+    private TextView tvTitle, tvAuthor, tvContent, tvDate;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        btn = findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        initialize();
+
+        loadPostData("qbD8eZasLTECYh5zGi41");
+    }
+
+    // Load Post
+    private void loadPostData(String postId) {
+        DocumentReference docRef = db.collection("post").document(postId);
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                bundle.putString("button_name", "MyButton");
-                mFirebaseAnalytics.logEvent("Button_Click", bundle);
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    // Check if exist document
+                    if (document.exists()) {
+                        String title = document.getString("title");
+                        String content = document.getString("content");
+                        Timestamp createdAt = document.getTimestamp("createdAt");
+                        String userId = document.getString("userId");
+
+                        // Format date
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+
+                        // Set data
+                        tvTitle.setText(title);
+                        tvContent.setText(content);
+                        tvDate.setText(sdf.format(createdAt.toDate()));
+
+                        // Load author
+                        loadAuthor(userId);
+                    } else { // Show if error
+                        Toast.makeText(MainActivity.this, "Lỗi không thể lấy dữ liệu", Toast.LENGTH_SHORT).show();
+                    }
+                } else { // Show if error
+                    Toast.makeText(MainActivity.this, "Lỗi không thể lấy dữ liệu", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    // Load author
+    private void loadAuthor(String userId) {
+        if (userId == null) return;
+
+        db.collection("user").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    if (document.exists()) {
+                        String userId = document.getString("username");
+                        tvAuthor.setText(userId);
+                    } else {
+                        tvAuthor.setText("Null");
+                    }
+                }
+            }
+        });
+    }
+
+    // Initial
+    private void initialize() {
+        // Initialize widgets
+        tvTitle = findViewById(R.id.tvTitle);
+        tvAuthor = findViewById(R.id.tvAuthor);
+        tvContent = findViewById(R.id.tvContent);
+        tvDate = findViewById(R.id.tvDate);
+
+
+        db = FirebaseFirestore.getInstance(); // Initialize database
     }
 }
