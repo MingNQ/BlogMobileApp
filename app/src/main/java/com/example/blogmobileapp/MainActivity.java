40bec150c8027bfddbe5b655c36ace27efce0c3f
Ming <minhlaminh01@gmail.com>
2025-02-23 22:18:14 +0700

    Coding Design

--- app/src/main/java/com/example/blogmobileapp/MainActivity.java
+++ app/src/main/java/com/example/blogmobileapp/MainActivity.java
@@ -28,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
             }
         });
+
+        // Go to Forget password Activity
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ForgetPasswordActivity.class));
+            }
+        });
     }
 
     // Initialize widgets
